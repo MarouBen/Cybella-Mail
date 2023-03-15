48b55bdc0ec7512d4759b2d7d0bc49279cfefd38
BEN ABBOU Marouane <maroumarocom@gmail.com>
2023-03-15 22:25:55 +0100

    send advancment

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
+  document.querySelector('#compose-form').addEventListener('submit', send_mail);
 
   // By default, load the inbox
   load_mailbox('inbox');
